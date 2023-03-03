23d646b64b8028da82b1fea04df047b7dbcec25e
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-03-03 10:40:03 +0300

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galaryContainer = document.querySelector('.gallery')
+
+const galereMarkup = createGalleryItem(galleryItems);
+
+galaryContainer.insertAdjacentHTML('beforeend', galereMarkup)
+
+console.log(galaryContainer)
+console.log(createGalleryItem(galleryItem));
+
+function createGalleryItem (galleryItem) {
+  const markup = galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt=""${description}"
+          />
+        </a>
+        </div>
+        `;
+  }).join('');
+  console.log(markup)
+return markup;
+ 
+}
